d3045a9c59e38db34fd5c58a46ff3e649387295b
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-09-29 19:44:25 +0300

    test

--- components/layout/wrapper.tsx
+++ components/layout/wrapper.tsx
@@ -1,13 +1,9 @@
 import { useEffect } from "react";
 import Header from "./header";
-import Sidebar from "./sidebar";
-import { useInjection } from "inversify-react";
-import { UserStore } from "../../stores/UserStore";
 import { observer } from "mobx-react";
-import { useRouter } from "next/router";
 import style from "./wrapper.module.scss";
-const Wrapper = observer(({ children }: any) => {
 
+const Wrapper = observer(({ children }: any) => {
   return (
     <div className={style.page__container}>
       <Header />
