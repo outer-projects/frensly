94f2df25000837ce5e9a0f4e946126f6aac278ae
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-10-05 23:40:27 +0300

    123

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,12 +16,13 @@ const Home: NextPage = observer((props) => {
   const { setUser, user } = useInjection(UserStore);
   const getUser = async () => {
     try {
-      const res = await fetch("https://frensly.adev.co/", {
-        method: "GET",
-        credentials: "include",
-      });
-      console.log(res);
-      setUser(res);
+      const res: AxiosRequestConfig = await axios.get(
+        "https://frensly.adev.co/",
+        {
+          withCredentials: true,
+        }
+      );
+      setUser(res.data);
     } catch (e) {
       console.log(e);
     }
