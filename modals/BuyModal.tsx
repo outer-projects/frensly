151fd2f6efd4bba198c4970352cc0e4ecffe44b2
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-10-09 20:51:11 +0300

    buy user

--- modals/BuyModal.tsx
+++ modals/BuyModal.tsx
@@ -8,6 +8,7 @@ import classNames from "classnames";
 import { ModalStore } from "../stores/ModalStore";
 import { useEffect, useState } from "react";
 import Web3Store from "../stores/Web3Store";
+import { fromWei } from "web3-utils";
 
 interface modalProps {
   key: ModalsEnum;
@@ -17,34 +18,51 @@ interface modalProps {
 
 export const BuyModal = observer(({ key, data, idx }: modalProps) => {
   const modalStore = useInjection(ModalStore);
-  const {frensly} = useInjection(Web3Store);
-  const [numberOfShares, setNumberOfShares] = useState(0);
+  const { frensly, address, checkAuth } = useInjection(Web3Store);
+  const [numberOfShares, setNumberOfShares] = useState<number | string>(0);
   const [currentPrice, setCurrentPrice] = useState(0);
-  const [priceOfOne, setPriceOfOne] = useState(0)
-  const checkPrice = async(num:number) =>{
+  const [priceOfOne, setPriceOfOne] = useState(0);
+  const buy = async() =>{
     try {
-      const res = await frensly.methods.getBuyPrice(data.user?.account?.address, num).call()
+      const res = await frensly.methods.buyShares(data.user?.account?.address, numberOfShares).send({
+        from: address,
+        value: currentPrice
+      })
       console.log(res);
-      return Number(res)
+      checkAuth()
+    } catch(e) {
+      console.log(e);
+    }
+    
+  }
+  const checkPrice = async (num: number) => {
+    try {
+      const res = await frensly.methods
+        .getBuyPrice(data.user?.account?.address, num)
+        .call();
+      console.log(res);
+      return Number(res);
     } catch (e) {
       console.log(e);
-      return 0
+      return 0;
     }
-  }
-  useEffect(()=>{
-    if(frensly) {
-      checkPrice(1).then((res)=>{
-        setPriceOfOne(res)
-      })
+  };
+  useEffect(() => {
+    if (frensly) {
+      checkPrice(1).then((res) => {
+        setPriceOfOne(res);
+      });
     }
-  },[frensly])
-  useEffect(()=>{
-    if(numberOfShares) {
-      checkPrice(numberOfShares).then((res)=>{
-        setCurrentPrice(res)
-      })
+  }, [frensly]);
+  useEffect(() => {
+    if (numberOfShares) {
+      checkPrice(
+        typeof numberOfShares == "number" ? numberOfShares : 0
+      ).then((res) => {
+        setCurrentPrice(res);
+      });
     }
-  },[numberOfShares])
+  }, [numberOfShares]);
   return (
     <ModalContainer heading={""} modalKey={key} idx={idx}>
       <div className={style.buy}>
@@ -66,12 +84,14 @@ export const BuyModal = observer(({ key, data, idx }: modalProps) => {
                 src="../icons/AvatarBlue.svg"
               />
               <div className={style.buy__user__left__text}>
-                <div className={style.buy__user__name}>{data.user?.twitterName}</div>
+                <div className={style.buy__user__name}>
+                  {data.user?.twitterName}
+                </div>
                 <div className={style.buy__status}>You own 0 keys</div>
               </div>
             </div>
             <div className={style.buy__user__left__text}>
-              <div className={style.buy__user__name}>{priceOfOne} ETH</div>
+              <div className={style.buy__user__name}>{fromWei(priceOfOne, 'ether')} ETH</div>
               <div className={style.buy__status}>
                 Key price <img src="../icons/Info.svg" />
               </div>
@@ -85,6 +105,8 @@ export const BuyModal = observer(({ key, data, idx }: modalProps) => {
               onChange={(e) => {
                 if (Number(e.target.value) >= 0.000001) {
                   setNumberOfShares(Number(e.target.value));
+                } else {
+                  setNumberOfShares("");
                 }
               }}
               type="number"
@@ -92,17 +114,19 @@ export const BuyModal = observer(({ key, data, idx }: modalProps) => {
           </div>
           <div className={style.buy__amount}>
             <div className={style.buy__amount__title}>Total ETH</div>
-            <div className={style.buy__amount__value}>{currentPrice} ETH</div>
+            <div className={style.buy__amount__value}>{fromWei(currentPrice, 'ether')} ETH</div>
           </div>
         </div>
         <div className={style.buy__buttons}>
-          <button
+          {/* <button
             className={classNames(header.connect__button, style.update__button)}
           >
             Update the price
-          </button>
+          </button> */}
           <button
             className={classNames(header.connect__button, style.buy__button)}
+            disabled={numberOfShares == 0 || numberOfShares == ''}
+            onClick={buy}
           >
             Buy
           </button>
