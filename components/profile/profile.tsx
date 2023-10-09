494525cdae27779ba04393b6a96d66b4e8d85fb1
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-10-09 22:25:54 +0300

    123

--- components/profile/profile.tsx
+++ components/profile/profile.tsx
@@ -118,7 +118,7 @@ const Profile = observer(() => {
             <div className={style.profile__stats__line}>
               <div className={style.profile__text}>Volume</div>
               <div className={classNames(style.profile__text, style.black)}>
-                {fromWei(profileUser?.account?.totalVolume as string, "szabo")} ETH
+                {/* {fromWei(profileUser?.account?.totalVolume as string, "szabo")} ETH */}
               </div>
             </div>
           </div>
