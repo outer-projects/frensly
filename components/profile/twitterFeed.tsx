3d30e288d0a59216c54247fcd3db126279e16009
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-10-11 18:39:51 +0300

    feed fix

--- components/profile/twitterFeed.tsx
+++ components/profile/twitterFeed.tsx
@@ -3,14 +3,14 @@ import MessageSend from "./messageSend";
 import TwitterPost from "./twitterPost";
 import { observer } from "mobx-react";
 import { useInjection } from "inversify-react";
-import FeedPage from "../../pages/feed";
 import { useEffect } from "react";
+import { FeedStore } from "../../stores/FeedStore";
 
 const TwitterFeed = observer(() => {
-  const { getPosts } = useInjection(FeedPage);
-  // useEffect(() => {
-  //   getPosts();
-  // }, []);
+  const { getPosts } = useInjection(FeedStore);
+  useEffect(() => {
+    getPosts();
+  }, []);
   return (
     <div className={style.twitter__feed}>
       <MessageSend />
