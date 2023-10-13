6d37d5eceeae1d03c072183127ef8438b7f627b0
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-10-13 12:49:11 +0300

    off sockets

--- utils/socket.ts
+++ utils/socket.ts
@@ -3,6 +3,6 @@ import React from 'react';
 import { io } from 'socket.io-client'
 
 export const socket = io('https://frensly.adev.co/api/v1/socket.io' as string ,{  
-    withCredentials: true
+    withCredentials: false
 });
 export const SocketContext = React.createContext(socket);
