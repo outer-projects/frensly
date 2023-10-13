8cbbe46b9f3dcf5bde2dcdba6b360efbea6d7794
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-10-13 16:09:51 +0300

    123

--- components/explore/exploreRow.tsx
+++ components/explore/exploreRow.tsx
@@ -17,6 +17,7 @@ const ExploreRow = observer(({ el }: { el: IProfile }) => {
       setUsdPrice(getPriceInUsd(el.account.currentPrice));
     }
   }, [el, ethCurrency]);
+  console.log(user?.account?.othersShares.filter((u) => u.subject == el._id));
   return (
     <Link href={"/profile/" + el.twitterId}>
       <div className={style.explore__user}>
@@ -25,7 +26,9 @@ const ExploreRow = observer(({ el }: { el: IProfile }) => {
           <div className={style.explore__user__left__text}>
             <div className={style.explore__user__share}>
               {/* @ts-ignore */}
-              {user?.account?.othersShares.filter((u) => u.subject == user._id && Number(u.amount) >= 1000000)?.length >= 1 && <img src="../icons/Key.svg" />}
+              {user?.account?.othersShares.filter(
+                (u) => u.subject == user._id && Number(u.amount) >= 1000000
+              )?.length >= 1 && <img src="../icons/Key.svg" />}
               <div>{Number(el?.account?.sharesAmount) / 10 ** 6} share</div>
             </div>
             <div className={style.explore__user__name}>{el.twitterName}</div>
