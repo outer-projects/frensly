2c735a7a4088a6c8c6951b2e0666b185917abf6a
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-10-13 13:31:47 +0300

    wss

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,7 +1,7 @@
 import React from "react";
 import { io } from "socket.io-client";
 
-export const socket = io("ws://frensly.adev.co" as string, {
+export const socket = io("wss://frensly.adev.co" as string, {
   transports: ["websocket"],
   withCredentials: true,
   path: "/api/v1/socket.io/",
