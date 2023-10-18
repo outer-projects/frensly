ae37d5bcf045c87299b95fdcf5b7d7a72fe93a66
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-10-18 22:45:47 +0300

    socials fix

--- components/socials/socials.tsx
+++ components/socials/socials.tsx
@@ -1,8 +1,8 @@
 import Twitter from "./twitter";
-import Discord from "./discord";
 import Telegram from "./telegram";
+import Medium from "./medium";
 export const socials = [
   { link: "https://twitter.com/frenslyio", logo: <Twitter/> },
-  { link: "https://twitter.com/frenslytech", logo: <Discord/> },
-  { link: "https://twitter.com/frenslytech", logo: <Telegram/> },
+  { link: "https://t.me/frenslyio", logo: <Telegram/> },
+  { link: "https://medium.com/@frensly", logo: <Medium/> },
 ];
--- components/socials/twitter.tsx
+++ components/socials/twitter.tsx
@@ -14,12 +14,14 @@ const Twitter = ({ color }: { color?: string }) => {
     >
       <path
         d="M1.78384 19.958H0L18.426 0.0209961H20.2938L1.78384 19.958Z"
-        fill={color ? color : hover ? "#151614" : "#D9D9D9"}
+        fill={color ? color : "#151614"}
+        stroke={color ? color : "#151614"}
+        strokeWidth={"0.5"}
       />
       <path
         d="M15.1525 19.5803L0.835976 0.437948L6.12841 0.420423L20.4832 19.5803H15.1525Z"
-        fill={color ? "#A6D000" : hover ? "white" : "#BBBEC7"}
-        stroke={color ? color : hover ? "#151614" : "transparent"}
+        fill={color ? "#B4B4B4" : "white"}
+        stroke={color ? color : "#151614"}
         strokeWidth={"1"}
       />
     </svg>
