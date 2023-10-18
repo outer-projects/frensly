3905f7972506f5f06a64adca0a9fd074ac500295
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-10-18 23:38:27 +0300

    test

--- components/ponds/chats/chat.tsx
+++ components/ponds/chats/chat.tsx
@@ -251,7 +251,7 @@ const Chat = observer(() => {
                           className={style.openchat__avatar}
                           src={el.user.avatar}
                         />}
-                        <div>
+                        <div className={style.openchat__msg_container}>
                           <div className={classNames(style.openchat__name,el.user.twitterId == user?.twitterId && style.openchat__mydate)}>
                             {el.user.twitterId !== user?.twitterId && (
                               <div>{el.user.twitterName}</div>
