30c5ba0f07be46a56b9f11e7f6e5522878926fda
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-10-22 21:05:21 +0300

    123

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -39,7 +39,7 @@ const Header = observer(() => {
     });
     socket.on("notification", (not: any) => {
       console.log("notification: ", not);
-      getNotifications();
+      // getNotifications();
       getUnreadCount();
     });
     // getNotifications();
