f23528646e5a20785a73f09607f36a9998a21e80
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-10-22 21:43:05 +0300

    123

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -4,12 +4,15 @@ import style from "./home.module.scss";
 import Explore from "../components/explore/explore";
 import TwitterFeed from "../components/profile/twitterFeed";
 import Head from "next/head";
+import { useInjection } from "inversify-react";
+import { UserStore } from "../stores/UserStore";
 
 const FeedPage: NextPage = observer((props) => {
+  const { unreadCount } = useInjection(UserStore);
   return (
     <div className={style.explore__page}>
       <Head>
-        <title>Frensly</title>
+        <title>{unreadCount !== 0 ? `(${unreadCount})` : ""}Frensly</title>
       </Head>
       <TwitterFeed />
     </div>
