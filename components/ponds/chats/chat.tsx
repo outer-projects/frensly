62116f1265f45f00a9d7883910962bf680c704e5
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-10-29 16:14:38 +0300

    room tesr

--- components/ponds/chats/chat.tsx
+++ components/ponds/chats/chat.tsx
@@ -63,8 +63,8 @@ const Chat = observer(() => {
         console.log(e, "hi connect_error");
       });
       socket.on("message", (msg) => {
-        console.log(msg, "hi message");
-        if (msg?.roomId == id) {
+        console.log(msg, "hi message", chat._id);
+        if (msg?.roomId == chat._id) {
           setNewMessage(msg);
           setMessagesLeft(messagesleft + 1)
         }
