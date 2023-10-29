2ffcf0ecc066af8a6ec1f0f28349c9cbe311e3e2
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-10-30 01:39:56 +0300

    123

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -32,7 +32,7 @@ const ExplorePage: NextPage = observer((props) => {
         </title>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
         />
       </Head>
       <Explore />
