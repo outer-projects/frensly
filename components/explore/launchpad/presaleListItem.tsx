6d0e54470b4722d09258421a956befe82f0f011c
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-11-18 21:36:22 +0300

    Refactor explore module and presale list item components

--- components/explore/launchpad/presaleListItem.tsx
+++ components/explore/launchpad/presaleListItem.tsx
@@ -2,19 +2,19 @@ import Link from "next/link";
 import { fromWeiToEth } from "../../../utils/utilities";
 import style from "../explore.module.scss";
 
-const PresaleListItem = (el: any) => {
+const PresaleListItem = ({presale}: any) => {
   return (
     <div className={style.presale__table__row}>
       <div className={style.row__1}>
-        <img src={el.image} />
+        <img src={presale.image} />
       </div>
-      <div className={style.row__2}>{el.name}</div>
-      <div className={style.row__3}>{Number(el.supply) / 10 ** 6}</div>
+      <div className={style.row__2}>{presale.name}</div>
+      <div className={style.row__3}>{Number(presale.supply) / 10 ** 6}</div>
       <div className={style.row__4}>Hardcap</div>
-      <div className={style.row__5}>{fromWeiToEth(el.price)}</div>
+      <div className={style.row__5}>{fromWeiToEth(presale.price)}</div>
       <div className={style.row__6}>Status</div>
       <div className={style.row__7}>Time to start</div>
-      <Link href={"/presale/" + el.handle}>
+      <Link href={"/presale/" + presale.handle}>
         <div className={style.row__8}>View</div>
       </Link>
     </div>
