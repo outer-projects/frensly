88650366a5a2345d42b670ae46783409223bf21d
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-11-18 19:47:08 +0300

    Fix color and add link to presale view

--- components/explore/launchpad/presaleListItem.tsx
+++ components/explore/launchpad/presaleListItem.tsx
@@ -1,16 +1,22 @@
+import Link from "next/link";
+import { fromWeiToEth } from "../../../utils/utilities";
 import style from "../explore.module.scss";
 
-const PresaleListItem = (el:any) => {
+const PresaleListItem = (el: any) => {
   return (
     <div className={style.presale__table__row}>
-      <div className={style.row__1}><img src="../avatar.svg"/></div>
-      <div className={style.row__2}>Name</div>
-      <div className={style.row__3}>Number of shares</div>
+      <div className={style.row__1}>
+        <img src={el.image} />
+      </div>
+      <div className={style.row__2}>{el.name}</div>
+      <div className={style.row__3}>{Number(el.supply) / 10 ** 6}</div>
       <div className={style.row__4}>Hardcap</div>
-      <div className={style.row__5}>Cost per share</div>
+      <div className={style.row__5}>{fromWeiToEth(el.price)}</div>
       <div className={style.row__6}>Status</div>
       <div className={style.row__7}>Time to start</div>
-      <div className={style.row__8}>View</div>
+      <Link href={"/presale/" + el.handle}>
+        <div className={style.row__8}>View</div>
+      </Link>
     </div>
   );
 };
