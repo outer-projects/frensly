e7a79f211e7148b71217039b1f5c539d283996cb
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-11-22 18:19:49 +0200

    Fix image source and add link to creator's profile

--- components/explore/launchpad/presaleListItem.tsx
+++ components/explore/launchpad/presaleListItem.tsx
@@ -2,16 +2,20 @@ import Link from "next/link";
 import { fromWeiToEth, getDate, getDateTime } from "../../../utils/utilities";
 import style from "../explore.module.scss";
 
-const PresaleListItem = ({presale}: any) => {
+const PresaleListItem = ({ presale }: any) => {
   return (
     <div className={style.presale__table__row}>
       <div className={style.row__1}>
-        <img src={presale?.image} />
+        <img src={presale?.image ? presale?.image : "../Avatar.svg"} />
       </div>
       <div className={style.row__2}>{presale?.name}</div>
       <div className={style.row__3}>{Number(presale?.supply) / 10 ** 6}</div>
       <div className={style.row__5}>{fromWeiToEth(presale?.price)}</div>
-      <div className={style.row__6}><Link href={'/profile/'+ presale?.creator?.profile?.twitterHandle}>{presale?.creator?.profile?.twitterName}</Link></div>
+      <div className={style.row__6}>
+        <Link href={"/profile/" + presale?.creator?.profile?.twitterHandle}>
+          {presale?.creator?.profile?.twitterName}
+        </Link>
+      </div>
       <div className={style.row__7}>{getDateTime(presale.presaleStart)}</div>
       <div className={style.row__7}>{getDateTime(presale.presaleEnd)}</div>
       <Link href={"/presales/" + presale?.handle}>
