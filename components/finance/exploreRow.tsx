2f456a2fede7dd630c1c00b40737c349162c29cb
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-11-24 15:57:43 +0200

    Fix exploreRow styling and remove unnecessary code

--- components/finance/exploreRow.tsx
+++ components/finance/exploreRow.tsx
@@ -10,14 +10,14 @@ import Web3Store from "../../stores/Web3Store";
 const CommunityRow = observer(({ el }: { el: any }) => {
   return (
     <Link href={"/communities/" + el.handle}>
-      <div className={style.explore__user}>
+      <div className={style.explore__user} style={{marginLeft:'24px', marginRight:"24px"}}>
         <div className={style.explore__user__left}>
           <img src={el.avatar} />
           <div className={style.explore__user__left__text}>
-            <div className={style.explore__user__share}></div>
+            
             <div className={style.explore__user__name}>
               <div>
-                {shortNick(el.name)}123 <span>Owner</span>
+                {shortNick(el.name)}123 
               </div>
               <span>
                 <a
@@ -28,11 +28,13 @@ const CommunityRow = observer(({ el }: { el: any }) => {
                     e.stopPropagation();
                   }}
                 >
-                  @{el.twitter}123
+                  Owner
                 </a>
               </span>
             </div>
+            <div>@{el.twitter}123</div>
           </div>
+
         </div>
         <div className={style.explore__user__right}>
           <div className={style.explore__user__price}>
