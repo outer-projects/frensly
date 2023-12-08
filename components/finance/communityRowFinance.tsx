719902ae47a3b7d578bd8d4fea81f17eee2e4736
alexweb3dev0x <alexweb3dev0x@proton.me>
2023-12-08 16:37:26 +0200

    Refactor communityRowFinance component

--- components/finance/communityRowFinance.tsx
+++ components/finance/communityRowFinance.tsx
@@ -10,39 +10,53 @@ import classNames from "classnames";
 
 const CommunityRow = observer(({ el, amount }: { el: any; amount: string }) => {
   return (
-    <Link href={"/communities/" + el.handle}>
-      <div
-        className={classNames(style.explore__user)}
-        style={{ marginLeft: "24px", marginRight: "24px", width: "auto" }}
-      >
-        <div className={style.explore__user__left}>
-          <img src={el.preview} />
-          <div className={style.explore__user__left__text}>
-            <div className={style.explore__user__name}>
-              <div>{shortNick(el.name)}</div>
-              <span>Owner</span>
+    <>
+      {el.handle ? (
+        <Link href={"/communities/" + el.handle}>
+          <div
+            className={classNames(style.explore__user)}
+            style={{ marginLeft: "24px", marginRight: "24px", width: "auto" }}
+          >
+            <div className={style.explore__user__left}>
+              <img src={el.preview} />
+              <div className={style.explore__user__left__text}>
+                <div className={style.explore__user__name}>
+                  <div>{shortNick(el.name)}</div>
+                  <span>Owner</span>
+                </div>
+                <div className={finance.finance__handle}>
+                  <a
+                    href={"https://twitter.com/" + el.twitter}
+                    target="_blank"
+                    rel="noreferrer"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                    }}
+                  >
+                    @{el.handle}
+                  </a>
+                </div>
+              </div>
             </div>
-            <div className={finance.finance__handle}>
-              <a
-                href={"https://twitter.com/" + el.twitter}
-                target="_blank"
-                rel="noreferrer"
-                onClick={(e) => {
-                  e.stopPropagation();
-                }}
-              >
-                @{el.handle}
-              </a>
+            <div className={style.explore__user__right}>
+              <div className={style.explore__user__price}>
+                <div>{Number(amount) / 10 ** 6} share</div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className={style.explore__user__right}>
-          <div className={style.explore__user__price}>
-            <div>{Number(amount) / 10 ** 6} share</div>
+        </Link>
+      ) : (
+        <Link href={"/communities/update/" + el.pondId}>
+          {" "}
+          <div
+            className={classNames(style.explore__user)}
+            style={{ marginLeft: "24px", marginRight: "24px", width: "auto" }}
+          >
+            Pond is not setted up yet. Click to set up.
           </div>
-        </div>
-      </div>
-    </Link>
+        </Link>
+      )}
+    </>
   );
 });
 export default CommunityRow;
